31713625bfea7cd4ff58908133491fe1752aa6d7
djhass <djhasselwander@gmail.com>
2025-04-04 22:33:27 -0400

    Improved Communication Between Files
    Asynchronous handles better

--- CathacksBackup/web_index.js
+++ CathacksBackup/web_index.js
@@ -1,22 +1,33 @@
 // const { exec } = import('child_process');
 // const { curly } = import('node-libcurl');
 
+let ID
+
 function show_output() {
     document.getElementById("AI Output").innerText = "AI Output for " + document.getElementById("to check for").value + " from " + document.getElementById("URL").value;
 };
 
-function getUrl() {
-    return document.getElementById("URL").value;
+function getInput() {
+    return document.getElementById("input").value;
 }
 
 
-// function post() {
-//     curly.post('localhost:3000/post', "EXAMPLEURL");
-// }
-
-function post() {
-    fetch("http://localhost:3000/url", {
+function postSubreddit() {
+    console.log("post")
+    fetch("http://localhost:3000/subreddit", {
         method: "POST",
-        body: getUrl(),
-      });
+        body: getInput(),
+      }).then(response => {
+        return response.text();
+      }).then(id => {
+        ID = id;
+      })
+}
+
+function getState() {
+    fetch(`http://localhost:3000/state/${ID}`, {
+        method: "GET",
+      }).then(response => {
+        console.log(response.text())
+      })
 }
