b4a2d7eb4f885b116c2a2943f2e8c06a6a2ade9c
djhass <djhasselwander@gmail.com>
2025-04-05 16:41:46 -0400

    improvements

--- CathacksBackup/redditscrapper.py
+++ CathacksBackup/redditscrapper.py
@@ -119,7 +119,7 @@ if validatesubred(subredname) == True:
         i = 0
         aicheck = 0
         aioutputs = []
-        print("AI is processing")
+        print("AI is processing...")
         for x in posts:
             input_text = x
             inputs = tokenizer(  
@@ -148,7 +148,7 @@ if validatesubred(subredname) == True:
             if (i > limit):
                 break
         ai_average = aicheck/i
-        print("writing the file")
+        print("Writing the file...")
         with open(f"response-{current_pid}", "w",encoding = "utf-8") as file:
             if (ai_average > 0.5):
             
@@ -161,7 +161,7 @@ if validatesubred(subredname) == True:
             else:
                 file.write("We believe there is not a considerable amount of AI generated content on the page")
     else:
-        print("Scraping Content")
+        print("Scraping Content...")
         #scrape website content
         comments = scrapecomments(subredname)
         posts = scrapecontent(subredname)
